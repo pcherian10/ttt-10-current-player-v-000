bb70e72d75dd797e056c3408ecc9fa0e6f3f6120
Paul <pcherian10@gmail.com>
2018-04-24 01:15:21 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,9 @@
+def turn_count (board)
+  counter = 0
+  board.each {|space| counter += 1 if (space != " " && space != "")}
+  counter
+end
+  
+def current_player (board)
+ turn_count(board).odd? ? "O" : "X"
+end
